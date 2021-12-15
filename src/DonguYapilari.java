7279d7a23acdd5640247635e8dd6e3aab0792a42
Baris Temiz <baristemiz334@gmail.com>
2021-12-16 00:07:42 +0300

    Initial commit

--- src/DonguYapilari.java
+++ src/DonguYapilari.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class DonguYapilari {
     public static void main(String[] args) {
+        int password;
+        boolean isPasswordTrue = false;
+        Scanner scanner = new Scanner(System.in);
 
+        while (!isPasswordTrue){
+            System.out.print("Şifre Giriniz: ");
+            password = scanner.nextInt();
+            if (password == 123) {
+                System.out.println("Şifre Doğru");
+                isPasswordTrue = true;
+            }
+            else {
+                System.out.println("Şifre Yanlış");
+            }
+        }
     }
 }
